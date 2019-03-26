642bd13af51bb7f256d298c2ca6c57bb387ae968
Rihards Dzerkals <rih.dze@gmail.com>
2019-03-26 09:28:06 +0200

    testingdropdown

--- TestingDropdown/app/src/main/java/com/example/testingdropdown/EditImageFragment.java
+++ TestingDropdown/app/src/main/java/com/example/testingdropdown/EditImageFragment.java
@@ -1,12 +1,22 @@
 package com.example.testingdropdown;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import com.zomato.photofilters.imageprocessors.Filter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -17,31 +27,52 @@ import android.view.ViewGroup;
  * Use the {@link EditImageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EditImageFragment extends Fragment {
+public class EditImageFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private EditImageFragmentListener listener;
+    Filter myfilter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
-    public EditImageFragment() {
-        // Required empty public constructor
+    Button selectImage;
+    Button saveImage;
+    ImageView imageView;
+    private int REQUEST_CODE = 1;
+    private Bitmap bitmap;
+//    private SeekBar seekBarBrightness;
+//    private SeekBar seekBarContrast;
+//    private SeekBar seekBarSaturation;
+    private PictureThread thread;
+
+
+
+    @BindView(R.id.seekbar_brightness)
+    SeekBar seekBarBrightness;
+
+    @BindView(R.id.seekbar_contrast)
+    SeekBar seekBarContrast;
+
+    @BindView(R.id.seekbar_saturation)
+    SeekBar seekBarSaturation;
+
+    EditImageFragment editImageFragment;
+
+    // modified image values
+
+
+
+    public void setListener(EditImageFragmentListener listener) {
+        this.listener = listener;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditImageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public EditImageFragment() {
+
+    }
+
+
     public static EditImageFragment newInstance(String param1, String param2) {
         EditImageFragment fragment = new EditImageFragment();
         Bundle args = new Bundle();
@@ -54,55 +85,143 @@ public class EditImageFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_image, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        View view = inflater.inflate(R.layout.fragment_edit_image, container, false);
+
+        ButterKnife.bind(this, view);
+
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.dog);
+        ImageView mLogo = (ImageView)getActivity().findViewById(R.id.image_preview);
+        mLogo.setImageBitmap(bitmap);
+
+//        imageView = view.findViewById(R.id.image_preview);
+//        imageView.setImageResource(R.drawable.dog);
+//        imageView.setImageBitmap(bitmap);
+//
+        thread = new PictureThread(mLogo,bitmap);
+        thread.start();
+
+
+
+        ButterKnife.bind(this, view);
+        seekBarBrightness.setMax(510);
+        seekBarBrightness.setProgress(255);
+
+        seekBarContrast.setMax(100);
+        seekBarContrast.setProgress(0);
+
+        seekBarSaturation.setMax(510);
+        seekBarSaturation.setProgress(255);
+
+        seekBarBrightness.setOnSeekBarChangeListener(this);
+        seekBarContrast.setOnSeekBarChangeListener(this);
+        seekBarSaturation.setOnSeekBarChangeListener(this);
+
+
+        seekBarBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+                thread.adjustBrightness(seekBar.getProgress()-100);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+            }
+
+
+        });
+
+
+
+        seekBarContrast.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                thread.adjustContrast(seekBar.getProgress());
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+        });
+
+
+        seekBarSaturation.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                thread.adjustSaturation(seekBar.getProgress()-255);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+        });
+
+        return view;
+
+
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+
+
+    public interface EditImageFragmentListener {
+        void onBrightnessChanged(int brightness);
+
+        void onSaturationChanged(float saturation);
+
+        void onContrastChanged(float contrast);
+
+        void onEditStarted();
+
+        void onEditCompleted();
     }
 }
--- TestingDropdown/app/src/main/java/com/example/testingdropdown/MainActivity.java
+++ TestingDropdown/app/src/main/java/com/example/testingdropdown/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.testingdropdown;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,21 +18,68 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.zomato.photofilters.imageprocessors.Filter;
+import com.zomato.photofilters.imageprocessors.subfilters.BrightnessSubFilter;
+import com.zomato.photofilters.imageprocessors.subfilters.ContrastSubFilter;
+import com.zomato.photofilters.imageprocessors.subfilters.SaturationSubfilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements EditImageFragment.OnFragmentInteractionListener, CropImage.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements EditImageFragment.EditImageFragmentListener, CropImage.OnFragmentInteractionListener{
     @BindView(R.id.viewpager)
     ViewPager viewPager;
     private TextView mTextMessage;
+//    @BindView(R.id.image_preview)
+//    ImageView imagePreview;
+    Bitmap bitmap;
+
+
+    @BindView(R.id.image_preview)
+    ImageView imageView;
+
+    int brightnessFinal = 0;
+    float saturationFinal = 1.0f;
+    float contrastFinal = 1.0f;
+
+
+    Bitmap originalImage;
+    // to backup image with filter applied
+    Bitmap filteredImage;
+
+    // the final image after applying
+    // brightness, saturation, contrast
+    Bitmap finalImage;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int SELECT_A_PHOTO = 2;
+
+    Button btn_take;
+    Button btn_list;
+    Button btn_load;
+    ImageView imageView_photo;
+
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.dog);
+        init();
+//        imageView = findViewById(R.id.image_preview);
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.addTab(tabLayout.newTab().setText("Edit Image"));
@@ -79,8 +132,192 @@ public class MainActivity extends AppCompatActivity implements EditImageFragment
     }
 
 
+
+
+
+
+
+
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+
+
+
+    @Override
+    public void onBrightnessChanged(final int brightness) {
+//        brightnessFinal = brightness;
+//        Filter myFilter = new Filter();
+//        myFilter.addSubFilter(new BrightnessSubFilter(brightness));
+//        imageView.setImageBitmap(myFilter.processFilter(finalImage.copy(Bitmap.Config.ARGB_8888, true)));
+    }
+
+    @Override
+    public void onSaturationChanged(final float saturation) {
+//        saturationFinal = saturation;
+//        Filter myFilter = new Filter();
+//        myFilter.addSubFilter(new SaturationSubfilter(saturation));
+//        imageView.setImageBitmap(myFilter.processFilter(finalImage.copy(Bitmap.Config.ARGB_8888, true)));
+    }
+
+    @Override
+    public void onContrastChanged(final float contrast) {
+//        contrastFinal = contrast;
+//        Filter myFilter = new Filter();
+//        myFilter.addSubFilter(new ContrastSubFilter(contrast));
+//        imageView.setImageBitmap(myFilter.processFilter(finalImage.copy(Bitmap.Config.ARGB_8888, true)));
+    }
+
+
+    @Override
+    public void onEditStarted() {
+
+    }
+
+    @Override
+    public void onEditCompleted() {
+
+        final Bitmap bitmap = filteredImage.copy(Bitmap.Config.ARGB_8888, true);
+
+        Filter myFilter = new Filter();
+        myFilter.addSubFilter(new BrightnessSubFilter(brightnessFinal));
+        myFilter.addSubFilter(new ContrastSubFilter(contrastFinal));
+        myFilter.addSubFilter(new SaturationSubfilter(saturationFinal));
+        finalImage = myFilter.processFilter(bitmap);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            dispatchTakePictureIntent();
+            return true;
+        }
+
+        if (id == R.id.action_save) {
+            galleryAddPic();
+            return true;
+        }
+        if (id == R.id.action_open){
+            //create the intent to take a photo from the gallery
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            //start the intent with a request code
+            startActivityForResult(intent, SELECT_A_PHOTO);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void initOnClickMethods(){
+
+        btn_take.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        btn_load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create the intent to take a photo from the gallery
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                //start the intent with a request code
+                startActivityForResult(intent, SELECT_A_PHOTO);
+
+            }
+        });
+
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                galleryAddPic();
+            }
+        });
+    }
+
+
+    public void init(){
+        btn_take = findViewById(R.id.action_settings);
+        btn_list = findViewById(R.id.action_save);
+        btn_load = findViewById(R.id.action_open);
+
+        imageView_photo = findViewById(R.id.image_preview);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            ImageView iv_photo;
+            iv_photo = findViewById(R.id.image_preview);
+
+            Glide.with(this).load(currentPhotoPath).into(iv_photo);
+        }
+
+        if (requestCode == SELECT_A_PHOTO && resultCode == RESULT_OK){
+            Uri selectedPhoto = data.getData();
+            ImageView iv_photo;
+            iv_photo = findViewById(R.id.image_preview);
+
+            Glide.with(this).load(selectedPhoto).into(iv_photo);
+        }
+    }
+
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Toast.makeText(this, "Something went wrong. Could not create file.", Toast.LENGTH_SHORT).show();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.testingdropdown.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,          /* prefix */
+                ".jpg",         /* suffix */
+                storageDir           /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 }
